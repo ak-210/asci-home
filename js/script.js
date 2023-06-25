2554374076d183badce8bf968b3f967789acd457
AK <akashkumarrai210@gmail.com>
2023-06-25 17:35:17 +0530

    updated contact form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){ //This function is the first one to run on load
+// $(window).onload = function() {
     var addData = function(dataLink, dataID) {
         dataID = '#' + dataID
         $ajaxUtils.sendGetRequest(
@@ -10,27 +10,31 @@ $(function(){ //This function is the first one to run on load
     };
 
     // Adding Data
+    // var loadContent = function() {
+    console.log("1")
     addData("snippets/carousel.html", "carousel");
     addData("snippets/case-study.html", "case-study");
     addData("snippets/team.html", "team-info");
-});
+// };
 
-window.onload = function () {
-// Working of the Carousel of the Case Study
 if(window.matchMedia("(min-width:576px)").matches){
+    console.log(2)
     // var carouselWidth = $("#case-study-inner")[0].scrollWidth;
-    var cardWidth = $("#case-study-inner .carousel-item").width();
+    // var cardWidth = $("#case-study #case-study-inner .carousel-item").width();
+
+    var cardWidth = $(window).width() / 3;
     var carouselWidth = 5*cardWidth
-    console.log(cardWidth, carouselWidth)
+    console.log(cardWidth, carouselWidth);
 
     
     var scrollPosition = 0
     
     $("#case-study .carousel-control-next").on('click', function(){
-        if(scrollPosition < (carouselWidth - (cardWidth*3.5))){
+        console.log("yes")
+        if((scrollPosition) < (carouselWidth - (cardWidth*3.5))){
             changeValues();
             scrollPosition = scrollPosition + cardWidth;
-            $("#case-study-inner").animate({scrollLeft: scrollPosition}, 600);
+            $("#case-study-inner").animate({scrollLeft: (scrollPosition)}, 600);
         }
     });
 
@@ -44,13 +48,14 @@ if(window.matchMedia("(min-width:576px)").matches){
 
     var changeValues = function(){
         var n = scrollPosition / cardWidth;
-        carouselWidth = $("#case-study-inner")[0].scrollWidth;
-        cardWidth = $("#case-study-inner .carousel-item").width();
+        cardWidth = $(window).width() / 3;
+        carouselWidth = 5*cardWidth;
 
         scrollPosition = n * cardWidth
     };
 
 }else{
     $("#case-study").addClass("slide");
-}
 };
+// };
+
